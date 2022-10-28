53e0953e89fe7c4674a8e3000943b22bd96717c3
dbeltran <daniel.beltran@bsc.es>
2022-10-28 13:36:03 +0200

    update_cmds were removed

--- setup.py
+++ setup.py
@@ -26,7 +26,6 @@ setuptools.setup(
                       'Pygments'],
     classifiers=[
         "Programming Language :: Python :: 3",
-        "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
     ],
 )
