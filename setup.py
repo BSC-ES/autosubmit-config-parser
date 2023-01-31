bf0a5a1d1e1079d7297bfda1ac7d8c67a2c7d96c
dbeltran <daniel.beltran@bsc.es>
2023-01-31 10:33:28 +0100

    typo in setup.py

--- setup.py
+++ setup.py
@@ -26,7 +26,7 @@ setuptools.setup(
                       'Pygments'],
     classifiers=[
         "Programming Language :: Python :: 3.7",
-        "Programming Language :: Python :: 3.9"
-        "Operating System :: OS Independent",
+        "Programming Language :: Python :: 3.9",
+        "Operating System :: OS Independent"
     ],
 )
