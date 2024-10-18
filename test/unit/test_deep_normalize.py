2f52d7967a92c41f6a623e32157a7b89f94e5b14
dbeltran <daniel.beltran@bsc.es>
2024-10-18 09:57:43 +0200

    Parametrized the test

--- test/unit/test_deep_normalize.py
+++ test/unit/test_deep_normalize.py
@@ -1,68 +1,71 @@
-def test_normalize_variables_for(autosubmit_config):
-    data = {
-        "JOBS": {
-            "job1": {
-                "FOR": {
-                    "NAME": "var",
-                    "lowercase": True
-                }
-            }
-        },
-        "var": ["test", "test2", "test3"]
-    }
-    expected_data = {
-        "JOBS": {
-            "JOB1": {
-                "FOR": {
-                    "NAME": "var",
-                    "LOWERCASE": True
-                }
-            }
-        },
-        "VAR": ["test", "test2", "test3"]
-    }
-    as_conf = autosubmit_config(expid='t000', experiment_data=data)
-    normalized_data = as_conf.deep_normalize(data)
-    assert normalized_data == expected_data
+import pytest
 
 
-def test_normalize_variables_for_with_lists(autosubmit_config):
-    data = {
-        "FOR": {
-            "DEPENDENCIES": [
-                {
-                    "APP_ENERGY_ONSHORE": {
-                        "SPLITS_FROM": {
-                            "all": {
-                                "SPLITS_TO": "previous"
+@pytest.mark.parametrize("data, expected_data", [
+    (
+        {
+            "JOBS": {
+                "job1": {
+                    "FOR": {
+                        "NAME": "var",
+                        "lowercase": True
+                    }
+                }
+            },
+            "var": ["test", "test2", "test3"]
+        },
+        {
+            "JOBS": {
+                "JOB1": {
+                    "FOR": {
+                        "NAME": "var",
+                        "LOWERCASE": True
+                    }
+                }
+            },
+            "VAR": ["test", "test2", "test3"]
+        }
+    ),
+    (
+        {
+            "FOR": {
+                "DEPENDENCIES": [
+                    {
+                        "APP_ENERGY_ONSHORE": {
+                            "SPLITS_FROM": {
+                                "all": {
+                                    "SPLITS_TO": "previous"
+                                }
                             }
                         }
-                    },
-                }
-            ],
+                    }
+                ]
+            },
+            "foo": ["bar", "baz"],
+            "1": ["one", "two"],
+            "3": "three"
         },
-        "foo": ["bar", "baz"],
-        "1": ["one", "two"],
-        "3": "three"
-    }
-    expected_data = {
-        "FOR": {
-            "DEPENDENCIES": [
-                {
-                    "APP_ENERGY_ONSHORE": {
-                        "SPLITS_FROM": {
-                            "ALL": {
-                                "SPLITS_TO": "previous"
+        {
+            "FOR": {
+                "DEPENDENCIES": [
+                    {
+                        "APP_ENERGY_ONSHORE": {
+                            "SPLITS_FROM": {
+                                "ALL": {
+                                    "SPLITS_TO": "previous"
+                                }
                             }
                         }
-                    },
-                }
-            ],
-        },
-        "FOO": ["bar", "baz"],
-        "1": ["one", "two"],
-        "3": "three"
-    }
+                    }
+                ]
+            },
+            "FOO": ["bar", "baz"],
+            "1": ["one", "two"],
+            "3": "three"
+        }
+    )
+])
+def test_normalize_variables(autosubmit_config, data, expected_data):
     as_conf = autosubmit_config(expid='t000', experiment_data=data)
     normalized_data = as_conf.deep_normalize(data)
     assert normalized_data == expected_data
