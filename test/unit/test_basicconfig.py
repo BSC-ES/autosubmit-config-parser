bf5eaf098377e3dc379d8ff496465010343ce43a
Irene Simo Munoz <irene.simomunoz@bsc.es>
2024-10-24 16:48:32 +0200

    Use fitxture from conftest.py for expid path tests #49

--- test/unit/test_basicconfig.py
+++ test/unit/test_basicconfig.py
@@ -1,6 +1,7 @@
 import os
 import pytest
 from autosubmitconfigparser.config.basicconfig import BasicConfig
+from conftest import autosubmit_config
 
 def test_read_file_config(tmp_path):
     config_content = """
@@ -21,36 +22,20 @@ def test_invalid_expid_path():
         for expid in invalid_expids:
             BasicConfig.expid_dir(expid)
 
-@pytest.fixture
-def temp_dir_expid(tmp_path):
-    # Create directory structure as Autosubmit.expid does
-    BasicConfig.LOCAL_ROOT_DIR = tmp_path 
-    exp_id = "a000"
-    exp_folder = os.path.join(tmp_path, exp_id)
-    os.mkdir(exp_folder)
-    os.mkdir(os.path.join(exp_folder, "conf"))
-    os.mkdir(os.path.join(exp_folder, "pkl"))
-    os.mkdir(os.path.join(exp_folder, "tmp"))
-    os.mkdir(os.path.join(exp_folder, "tmp", "ASLOGS"))
-    os.mkdir(os.path.join(exp_folder, "tmp", "LOG_"+exp_id))
-    os.mkdir(os.path.join(exp_folder, "plot"))
-    os.mkdir(os.path.join(exp_folder, "status"))
-    # end Autosubmit.expid
-    return tmp_path, exp_id
-
 functions_expid = [BasicConfig.expid_dir,
                    BasicConfig.expid_tmp_dir,
                    BasicConfig.expid_log_dir,
                    BasicConfig.expid_aslog_dir]
 root_dirs = [
-    lambda root_path, exp_id: str(root_path / exp_id),
-    lambda root_path, exp_id: str(root_path / exp_id / "tmp"),
-    lambda root_path, exp_id: str(root_path / exp_id / "tmp" / f"LOG_{exp_id}"),
-    lambda root_path, exp_id: str(root_path / exp_id / "tmp" / "ASLOGS")
+    lambda root_path, exp_id: os.path.join(root_path, exp_id),
+    lambda root_path, exp_id: os.path.join(root_path, exp_id, "tmp"),
+    lambda root_path, exp_id: os.path.join(root_path, exp_id, "tmp", f"LOG_{exp_id}"),
+    lambda root_path, exp_id: os.path.join(root_path, exp_id, "tmp", "ASLOGS")
 ]
 @pytest.mark.parametrize("foo, dir_func", zip(functions_expid, root_dirs))
-def test_expid_dir_structure(foo, dir_func, temp_dir_expid):
-    root_path, exp_id = temp_dir_expid
+def test_expid_dir_structure(foo, dir_func, autosubmit_config):
+    exp_id = 'a000'
+    root_path = autosubmit_config(expid = exp_id).basic_config.LOCAL_ROOT_DIR
     expected_path = dir_func(root_path, exp_id)
     result = foo(exp_id)
     assert result == expected_path
