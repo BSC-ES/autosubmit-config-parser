55f21781f2b6568b55823471d8b293f610435536
Irene Simo Munoz <irene.simomunoz@bsc.es>
2024-10-24 13:07:35 +0200

    Fix returned object type for expid path functions #48
    Modified scripts to return str instead of PosixPath  and avoid extra dependency on pathlib

--- autosubmitconfigparser/config/basicconfig.py
+++ autosubmitconfigparser/config/basicconfig.py
@@ -23,7 +23,6 @@ except ImportError:
     # noinspection PyCompatibility
     from configparser import ConfigParser as SafeConfigParser
 import os
-from pathlib import Path
 import inspect
 
 
@@ -76,19 +75,19 @@ class BasicConfig:
     def EXPID_DIR(exp_id):
         if not isinstance(exp_id, str) or len(exp_id) != 4:
             raise TypeError("Experiment ID must be a string of 4 characters")
-        return Path(BasicConfig.LOCAL_ROOT_DIR, exp_id)
+        return os.path.join(BasicConfig.LOCAL_ROOT_DIR, exp_id)
 
     @staticmethod
     def EXPID_TMP_DIR(exp_id):
-        return BasicConfig.EXPID_DIR(exp_id) / BasicConfig.LOCAL_TMP_DIR 
+        return os.path.join(BasicConfig.EXPID_DIR(exp_id), BasicConfig.LOCAL_TMP_DIR) 
 
     @staticmethod
     def EXPID_LOG_DIR(exp_id):
-        return BasicConfig.EXPID_TMP_DIR(exp_id) / f'LOG_{exp_id}'
+        return os.path.join(BasicConfig.EXPID_TMP_DIR(exp_id), f'LOG_{exp_id}')
 
     @staticmethod
     def EXPID_ASLOG_DIR(exp_id):
-        return BasicConfig.EXPID_TMP_DIR(exp_id) / BasicConfig.LOCAL_ASLOG_DIR 
+        return os.path.join(BasicConfig.EXPID_TMP_DIR(exp_id), BasicConfig.LOCAL_ASLOG_DIR)
     
     @staticmethod
     def _update_config():
