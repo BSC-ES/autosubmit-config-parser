71254afd4bfc980edf919cb2ae2b4068f32fe044
dbeltrankyl <daniel.beltran@bsc.es>
2025-03-05 16:50:09 +0100

    Added test for needs_reload

---
+++ test/unit/test_reload.py
@@ -0,0 +1,53 @@
+import time
+from pathlib import Path
+
+import pytest
+
+
+@pytest.mark.parametrize("force_load, current_loaded_files, expected_result", [
+    (True, None, ['%NOTFOUND%', '%TEST%', '%TEST2%']),
+    (True, None, ['%NOTFOUND%', '%TEST%', '%TEST2%']),
+    (False, "older", ['%NOTFOUND%', '%TEST%', '%TEST2%']),
+    (False, "newer", None),
+    (True, "older", ['%NOTFOUND%', '%TEST%', '%TEST2%']),
+    (True, "newer", ['%NOTFOUND%', '%TEST%', '%TEST2%']),
+])
+def test_reload(autosubmit_config, force_load, current_loaded_files, expected_result):
+    as_conf = autosubmit_config(
+        expid='a000',
+        experiment_data={})
+    Path(as_conf.conf_folder_yaml).mkdir(parents=True, exist_ok=True)
+
+    with open(as_conf.conf_folder_yaml / 'test.yml', 'w') as f:
+        f.write('VAR: ["%NOTFOUND%", "%TEST%", "%TEST2%"]')
+    if current_loaded_files:
+        if current_loaded_files == "older":
+            as_conf.current_loaded_files[as_conf.conf_folder_yaml / 'test.yml'] = time.time() - 1000
+        else:
+            as_conf.current_loaded_files[as_conf.conf_folder_yaml / 'test.yml'] = time.time() + 1000
+    as_conf.reload(force_load=force_load)
+    if expected_result:
+        assert as_conf.experiment_data['VAR'] == expected_result
+    else:
+        assert as_conf.experiment_data.get('VAR', None) is None
+
+
+@pytest.mark.parametrize("current_loaded_files, expected_result", [
+    (None, True),
+    ("older", True),
+    ("newer", False),
+])
+def test_needs_reload(autosubmit_config, current_loaded_files, expected_result):
+    as_conf = autosubmit_config(
+        expid='a000',
+        experiment_data={})
+    Path(as_conf.conf_folder_yaml).mkdir(parents=True, exist_ok=True)
+
+    with open(as_conf.conf_folder_yaml / 'test.yml', 'w') as f:
+        f.write('VAR: ["%NOTFOUND%", "%TEST%", "%TEST2%"]')
+    if current_loaded_files:
+        if current_loaded_files == "older":
+            as_conf.current_loaded_files[as_conf.conf_folder_yaml / 'test.yml'] = time.time() - 1000
+        else:
+            as_conf.current_loaded_files[as_conf.conf_folder_yaml / 'test.yml'] = time.time() + 1000
+    assert as_conf.needs_reload() == expected_result
