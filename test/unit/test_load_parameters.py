4646e1e0c0b086e3c6013dd8d57f03d0dd18d84e
dbeltrankyl <daniel.beltran@bsc.es>
2025-03-06 23:32:24 +0100

    Less leaks

--- test/unit/test_load_parameters.py
+++ test/unit/test_load_parameters.py
@@ -2,7 +2,7 @@ def test_load_parameters(autosubmit_config):
 
     as_conf = autosubmit_config(
         expid='a000',
-        experiment_data={'VAR': {"deep_var": ["%NOTFOUND%", "%TEST%", "%TEST2%"]}})
+        experiment_data={'VAR': {"DEEP_VAR": ["%NOTFOUND%", "%TEST%", "%TEST2%"]}})
     as_conf.experiment_data.update({'d': '%d%', 'd_': '%d_%', 'Y': '%Y%', 'Y_': '%Y_%',
                                    'M': '%M%', 'M_': '%M_%', 'm': '%m%', 'm_': '%m_%'})
     parameters = as_conf.load_parameters()
