30cf39cce57f0480ebe78a4f1c35307696b8f3d2
dbeltrankyl <daniel.beltran@bsc.es>
2025-03-14 12:33:50 +0100

    duplicate imports fix

--- test/unit/test_save.py
+++ test/unit/test_save.py
@@ -2,10 +2,6 @@ import pytest
 from pathlib import Path
 from ruamel.yaml import YAML
 
-import pytest
-from pathlib import Path
-from ruamel.yaml import YAML
-
 
 @pytest.mark.parametrize("data, owner", [
     ({
