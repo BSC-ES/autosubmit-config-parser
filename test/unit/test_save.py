798157e7766ba7527e30fb9a3e5b3bbfe2dea3e2
dbeltrankyl <daniel.beltran@bsc.es>
2025-03-14 12:31:59 +0100

    improve test

--- test/unit/test_save.py
+++ test/unit/test_save.py
@@ -2,33 +2,50 @@ import pytest
 from pathlib import Path
 from ruamel.yaml import YAML
 
+import pytest
+from pathlib import Path
+from ruamel.yaml import YAML
 
-@pytest.mark.parametrize("data", [
-    {
-        "DEFAULT": {
-            "HPCARCH": "local",
-        },
-    },
-], ids=["local"])
-def test_save(autosubmit_config, tmpdir, data, mocker):
+
+@pytest.mark.parametrize("data, owner", [
+    ({
+         "DEFAULT": {
+             "HPCARCH": "local",
+         },
+     }, True),
+    ({
+         "DEFAULT": {
+             "HPCARCH": "local",
+         },
+     }, False)
+], ids=["local_true", "local_false"])
+def test_save(autosubmit_config, tmpdir, mocker, data, owner):
     data['ROOTDIR'] = tmpdir.strpath
-    data['AS_ENV_CURRENT_USER'] = Path(tmpdir).owner()
+    if owner:
+        data['AS_ENV_CURRENT_USER'] = Path(tmpdir).owner()
+    else:
+        data['AS_ENV_CURRENT_USER'] = 'whatever'
     as_conf = autosubmit_config(expid='t000', experiment_data=data)
     as_conf.save()
-    assert Path(as_conf.metadata_folder) / 'experiment_data.yml' in Path(as_conf.metadata_folder).iterdir()
-    # check contents
-    with open(Path(as_conf.metadata_folder) / 'experiment_data.yml', 'r') as f:
-        yaml = YAML(typ="safe")
-        assert data == yaml.load(f)
+    if not owner:
+        assert not Path(as_conf.metadata_folder).exists()
+    else:
+        assert Path(as_conf.metadata_folder).exists()
+        assert Path(as_conf.metadata_folder) / 'experiment_data.yml' in Path(as_conf.metadata_folder).iterdir()
 
-    # Test .bak generated.
-    as_conf.save()
-    assert Path(as_conf.metadata_folder) / 'experiment_data.yml.bak' in Path(as_conf.metadata_folder).iterdir()
+        # check contents
+        with open(Path(as_conf.metadata_folder) / 'experiment_data.yml', 'r') as f:
+            yaml = YAML(typ="safe")
+            assert data == yaml.load(f)
 
-    # force fail save
-    mocker.patch("builtins.open", side_effect=Exception("Forced exception"))
-    mocker.patch("shutil.copyfile", return_value=True)
-    as_conf.save()
-    assert Path(as_conf.metadata_folder) / 'experiment_data.yml' not in Path(as_conf.metadata_folder).iterdir()
-    assert as_conf.data_changed is True
-    assert as_conf.last_experiment_data == {}
+        # Test .bak generated.
+        as_conf.save()
+        assert Path(as_conf.metadata_folder) / 'experiment_data.yml.bak' in Path(as_conf.metadata_folder).iterdir()
+
+        # force fail save
+        mocker.patch("builtins.open", side_effect=Exception("Forced exception"))
+        mocker.patch("shutil.copyfile", return_value=True)
+        as_conf.save()
+        assert Path(as_conf.metadata_folder) / 'experiment_data.yml' not in Path(as_conf.metadata_folder).iterdir()
+        assert as_conf.data_changed is True
+        assert as_conf.last_experiment_data == {}
