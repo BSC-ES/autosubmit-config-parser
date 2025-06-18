f6a2b6109b0b5e3ab46085440ad225bfdb91cccf
dbeltrankyl <daniel.beltran@bsc.es>
2025-06-18 16:15:21 +0200

    tests fixing bugs

--- autosubmitconfigparser/config/configcommon.py
+++ autosubmitconfigparser/config/configcommon.py
@@ -671,11 +671,11 @@ class AutosubmitConfig(object):
                 aux_dependencies[dependency.upper()] = dependency_data
                 if type(dependency_data) is dict and dependency_data.get("STATUS", None):
                     dependency_data["STATUS"] = dependency_data["STATUS"].upper()
+                    dependency_data["OPTIONAL"] = dependency_data.get("OPTIONAL", False)
                     if dependency_data["STATUS"][-1] == "?":
                         dependency_data["STATUS"] = dependency_data["STATUS"][:-1]
                         dependency_data["OPTIONAL"] = True
-                    else:
-                        dependency_data["OPTIONAL"] = False
+
 
         return aux_dependencies
 
